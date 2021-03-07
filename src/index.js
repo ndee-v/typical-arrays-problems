dc64cfc376f6ebe60c098fa17b8cbe0841cb35b6
ndee-v <dronez86@gmail.com>
2021-03-07 17:22:49 +0600

    refactor: fill functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof(array) == "undefined" || array === null || array.length == 0) {
+    return 0
+  }
+  let min = array[0]
+
+  array.forEach(element => {
+    if (element < min) {
+      min = element
+    }
+  });
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof(array) == "undefined" || array === null || array.length == 0) {
+    return 0
+  }
+  let max = array[0]
+
+  array.forEach(element => {
+    if (element > max) {
+      max = element
+    }
+  });
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof(array) == "undefined" || array === null || array.length == 0) {
+    return 0
+  }
+  
+  let sum = array.reduce((accum, elem) => accum += elem, 0)
+
+  return sum / array.length
 }
